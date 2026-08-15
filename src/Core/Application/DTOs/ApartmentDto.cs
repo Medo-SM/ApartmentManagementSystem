d31e275334c207b28ce15b482ada9832365c9c9b
Medo-SM <malsmawi1@gmail.com>
2026-08-15 18:16:25 +0300

    Feat(Infrastructure): Added the DTO layer for tenant, user and apartment entities

---
+++ src/Core/Application/DTOs/ApartmentDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    class ApartmentDto
+    {
+        int Id { get; set; }
+        string UnitNumber { get; set; }
+        int FloorNumber{ get; set; }
+        int NumberOfRooms { get; set; }
+        decimal MonthlyRent { get; set; }
+        string OccupancyStatus { get; set; } = "Vacant";
+        int? CurrentTenantId{ get; set; }
+    }
+}
---
+++ src/Core/Application/DTOs/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Application.DTOs
+{
+    public class UserDto
+
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public int RoleId { get; set; }
+        public int? TenantId { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
